ff4d2f91d80b874032c83da5fafc79c6b01f4f8f
Aaron Imbrock <aimbrock@gmail.com>
2023-08-11 14:49:44 -0700

    stage 2 check

--- app/main.py
+++ app/main.py
@@ -7,13 +7,14 @@ PORT = 6379
 def main():
     print("Logs from your program will appear here!")
 
+    pong = "+PONG\r\n"
 
     server_socket = socket.create_server((HOST, PORT), reuse_port=True)
     conn, addr = server_socket.accept()
 
     with conn:
         conn.recv(1024)
-        conn.send("+PONG", end="\r\n".encode())
+        conn.send(pong.encode())
 
 if __name__ == "__main__":
     main()
